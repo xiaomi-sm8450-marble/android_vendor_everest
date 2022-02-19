b0b46d31a321c229045d0323b6ccf0751762fbac
jhonboy121 <alfredmathew05@gmail.com>
2022-02-19 14:46:25 +0900

    vendor: Introduce App Lock [4/4]
    Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

--- config/common.mk
+++ config/common.mk
@@ -168,6 +168,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/everest/prebuilt/common/etc/init/init.openssh.rc:$(TARGET_COPY_OUT_PRODUCT)/etc/init/init.openssh.rc
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/everest/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
