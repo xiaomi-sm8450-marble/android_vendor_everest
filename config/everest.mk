45c4019d161eea0e507bd2cbe2a6c7bd3eaaa294
janak <niraulajanak2019@gmail.com>
2024-03-04 04:01:49 +0000

    vendor: Build GAPPS conditionally

--- config/everest.mk
+++ config/everest.mk
@@ -17,7 +17,10 @@ PRODUCT_PACKAGES += \
     EverestWallpaperStub
 
 # Gapps
+WITH_GAPPS ?= true
+ifeq ($(WITH_GAPPS),true)
 $(call inherit-product-if-exists, vendor/gms/products/gms.mk)
+endif
 
 # repainter
 PRODUCT_PACKAGES += \
